c66a30cfa214eabaa5d6bc9c51b7bba3715c1642
Ethan Smith <edhsmith@icloud.com>
2021-03-11 07:56:17 +1300

    Initial Commit

---
+++ Me/MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeApp.swift
+//  Me
+//
+//  Created by Ethan Smith on 11/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
